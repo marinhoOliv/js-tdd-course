1f02b6c64fb90ad4c6a5f97e0349937632710b8b
Mario Antonio de Oliveira Junior <mario.oliveira@e-construmarket.com.br>
2017-10-26 15:34:14 -0200

    add config files

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const a = 3;
+const b = 2;
+
+const c = 'slfgjshhsaghkjsdhgkhdshgkjdhsgkjhdkjhgkjhdsjhgjkshdkjghjkdshgkdshgkdshgkjdhgjkdhgdhskdhskghk';
+
+function sum() {
+  return a + b + c;
+}
+
+sum();
